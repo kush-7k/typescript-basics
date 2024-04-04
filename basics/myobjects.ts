54cb98564e44cf2fd7cc63c160bdc3abad717912
Kush Patel <113313092+kush-7k@users.noreply.github.com>
2024-04-04 16:53:08 -0400

    aliasing

--- basics/myobjects.ts
+++ basics/myobjects.ts
@@ -14,4 +14,47 @@ const User = {
  function createCourse():{name:string, price:number}{
     return{name: "reactJS", price: 399}
  }
+
+
+ //You can make your own types by using keyword type
+ //Also you can alias all the datatypes as well and give it your own name however it is not useful
+ type User = { 
+    name: string;
+    email: string;
+    isActive: boolean;
+
+ }
+ //aliasing
+ type myString= string
+
+ function createUser1(user: User){
+ return(User);
+ }
+
+ createUser1({name: "", email:"", isActive: true})
 export{}
+
+
+type User12 = { 
+    readonly _id: string
+    name: string;
+    email: string;
+    isActive: boolean;
+    creditCard?: string;
+}
+
+function createUser12(u:User12){
+
+}
+
+let myUser: User12 ={
+    _id: "123",
+    name: "h",
+    email: "jasm",
+    isActive: false
+    
+}
+
+myUser.email = "k@keyof.cn"
+//This is error
+//myUser.id = 12 as it was readonly
